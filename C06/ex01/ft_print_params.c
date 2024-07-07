fabea8b34b4653f533de66fe116626bcdef784be
pjaguin <jaguinpaul@gmail.com>
2024-07-07 14:56:30 +0200

    Update the C06

--- C06/ex01/ft_print_params.c
+++ C06/ex01/ft_print_params.c
@@ -1,22 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pjaguin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/04 15:38:20 by pjaguin           #+#    #+#             */
-/*   Updated: 2024/07/05 13:16:38 by pjaguin          ###   ########.fr       */
+/*   Created: 2024/07/05 13:19:12 by pjaguin           #+#    #+#             */
+/*   Updated: 2024/07/05 13:25:27 by pjaguin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+
 int	ft_strlen(char *str)
 {
 	int	i;
 
 	i = 0;
-	while(str[i] != 0)
+	while(str[i])
 	{
 		i++;
 	}
@@ -25,9 +26,18 @@ int	ft_strlen(char *str)
 
 int	main(int argc, char** argv)
 {
-	(void)argc;
-	char	*name;
+	int	i;
 
-	name = argv[0];
-	write(1, name, ft_strlen(name));
+	i = 1;
+
+	(void)argc; 
+
+	while(argv[i])
+	{
+		write(1, argv[i], ft_strlen(argv[i]));
+		write(1, "\n" ,1);
+		i++;
+	}
+
+	return (0);
 }
--- C06/ex02/ft_rev_params.c
+++ C06/ex02/ft_rev_params.c
@@ -1,33 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pjaguin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/04 15:38:20 by pjaguin           #+#    #+#             */
-/*   Updated: 2024/07/05 13:16:38 by pjaguin          ###   ########.fr       */
+/*   Created: 2024/07/05 13:26:22 by pjaguin           #+#    #+#             */
+/*   Updated: 2024/07/05 14:25:56 by pjaguin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
+
 int	ft_strlen(char *str)
 {
 	int	i;
 
 	i = 0;
-	while(str[i] != 0)
+	while(str[i])
 	{
 		i++;
 	}
 	return (i);
 }
 
-int	main(int argc, char** argv)
+int	main(int argc, char **argv)
 {
-	(void)argc;
-	char	*name;
+	int	i;		
 
-	name = argv[0];
-	write(1, name, ft_strlen(name));
+	i = argc - 1;
+	while (i > 0)
+	{
+		write(1, argv[i], ft_strlen(argv[i]));
+		write(1, "\n", 1);
+		i--;
+	}
+	return (0);
 }
