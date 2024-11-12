a8f41d2f1e6d8ceedb501f06530b8f1903f992b9
pjaguin <jaguinpaul@gmail.com>
2024-11-12 09:52:43 +0100

    run some tests

--- libft/main.c
+++ libft/main.c
@@ -6,15 +6,43 @@
 /*   By: pjaguin <pjaguin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 14:11:52 by pjaguin           #+#    #+#             */
-/*   Updated: 2024/11/08 16:58:29 by pjaguin          ###   ########.fr       */
+/*   Updated: 2024/11/12 09:44:54 by pjaguin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <ctype.h>
 #include <stdio.h>
 
-int		main(void)
+int	ft_check_func_is(int(ft)(int), int(oem)(int), char *ft_name)
 {
-	printf("%c", ft_tolower('Q'));
-	printf("%c", ft_toupper('{'));
+	int	i;
+	int	end;
+
+	i = -10;
+	end = 120;
+	while (i <= end)
+	{
+		if (ft(i) == oem(i))
+			printf("✅ Alles gut for %s values: %i and %i\n ", ft_name, ft(i), oem(i));
+		else if (ft(i) > 0 && oem(i) > 0)
+			printf("✅ Alles gut for %s values: %i and %i\n ", ft_name, ft(i), oem(i));
+		else
+		{
+			printf("❌ Error with %s values: %i and %i for i: %i\n", ft_name, ft(i), oem(i), i);
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	main(void)
+{
+	if (!ft_check_func_is(ft_isalnum, isalnum, "ft_isalnum"))
+		return (0);
+	if (!ft_check_func_is(ft_isalpha, isalpha, "ft_isalpha"))
+		return (0);
+	if (!ft_check_func_is(ft_isascii, isascii, "ft_isascii"))
+		return (0);
 }
