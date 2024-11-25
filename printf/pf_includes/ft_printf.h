7635e8ec1e94abe0f18ca58a651fa1bb163af8a8
pjaguin <jaguinpaul@gmail.com>
2024-11-25 11:26:58 +0100

    2024-11-25 11:26:58 autosave printf

--- printf/pf_includes/ft_printf.h
+++ printf/pf_includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: pjaguin <pjaguin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 12:49:30 by pjaguin           #+#    #+#             */
-/*   Updated: 2024/11/22 13:31:02 by pjaguin          ###   ########.fr       */
+/*   Updated: 2024/11/25 11:22:21 by pjaguin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_printf(const char *str, ...);
 int	ft_print_string_count(char *str, int fd);
 int	ft_print_number_count(int n);
 int	ft_print_unsigned_count(unsigned int n);
-int	ft_count_putptr_fd(unsigned long ptr, int fd);
+int	ft_count_putptr_fd(unsigned long int ptr, int fd);
 int	ft_count_putnbr_base_fd(unsigned int nbr, char type, int fd);
 
 #endif
