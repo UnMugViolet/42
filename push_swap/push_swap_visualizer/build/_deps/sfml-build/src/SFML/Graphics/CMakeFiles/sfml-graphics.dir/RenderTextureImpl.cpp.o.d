a7afcf8967b7ecbe2b8acbef77910743dd9df6ab
unmugviolet <paul@rocketegg.systems>
2025-01-20 12:19:25 +0100

    remove all the trash from the visualizer

--- push_swap/push_swap_visualizer/build/_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderTextureImpl.cpp.o.d
+++
@@ -1,7 +0,0 @@
-_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderTextureImpl.cpp.o: \
- /home/unmugviolet/Desktop/42/push_swap/push_swap_visualizer/build/_deps/sfml-src/src/SFML/Graphics/RenderTextureImpl.cpp \
- /usr/include/stdc-predef.h \
- /home/unmugviolet/Desktop/42/push_swap/push_swap_visualizer/build/_deps/sfml-src/src/SFML/Graphics/RenderTextureImpl.hpp \
- /home/unmugviolet/Desktop/42/push_swap/push_swap_visualizer/build/_deps/sfml-src/include/SFML/System/NonCopyable.hpp \
- /home/unmugviolet/Desktop/42/push_swap/push_swap_visualizer/build/_deps/sfml-src/include/SFML/System/Export.hpp \
- /home/unmugviolet/Desktop/42/push_swap/push_swap_visualizer/build/_deps/sfml-src/include/SFML/Config.hpp
