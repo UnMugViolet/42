aeba4c1697830ae94ec6c3d632712ff15b5ae6b3
unmugviolet <paul@rocketegg.systems>
2025-01-22 12:37:54 +0100

    Update gnl add typo on the strjoin

--- push_swap/libft/includes/libft.h
+++ push_swap/libft/includes/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pjaguin <pjaguin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: unmugviolet <unmugviolet@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 13:07:27 by pjaguin           #+#    #+#             */
-/*   Updated: 2025/01/14 17:04:53 by pjaguin          ###   ########.fr       */
+/*   Updated: 2025/01/22 12:36:09 by unmugviolet      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,7 @@ void				*ft_calloc(size_t nmemb, size_t size);
 char				*ft_strdup(const char *str);
 char				*ft_substr(char const *str, unsigned int start, size_t len);
 char				*ft_strjoin(char const *s1, char const *s2);
+char				*ft_strjoin_free(char *s1, char *s2);
 char				*ft_strtrim(char const *s1, char const *set);
 char				**ft_split(char const *str, char c);
 char				*ft_itoa(int n);
--- push_swap/libft/srcs/gnl/get_next_line_utils.c
+++ push_swap/libft/srcs/gnl/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: unmugviolet <unmugviolet@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 18:14:26 by pjaguin           #+#    #+#             */
-/*   Updated: 2025/01/22 10:56:08 by unmugviolet      ###   ########.fr       */
+/*   Updated: 2025/01/22 12:34:56 by unmugviolet      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,7 +89,7 @@ char	*read_file(int fd, char *main_str)
 			return (NULL);
 		}
 		buff[count] = 0;
-		main_str = ft_strjoin(main_str, buff);
+		main_str = ft_strjoin_free(main_str, buff);
 	}
 	free(buff);
 	return (main_str);
