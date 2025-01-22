442b79693fa7ff047aa758fd12b6de658e51513b
unmugviolet <paul@rocketegg.systems>
2025-01-22 17:14:55 +0100

    Almost done

--- push_swap/ps_srcs/checker/checker.c
+++ push_swap/ps_srcs/checker/checker.c
@@ -6,14 +6,21 @@
 /*   By: unmugviolet <unmugviolet@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 09:25:15 by unmugviolet       #+#    #+#             */
-/*   Updated: 2025/01/22 13:42:05 by unmugviolet      ###   ########.fr       */
+/*   Updated: 2025/01/22 17:12:34 by unmugviolet      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "push_swap.h"
+#include <stdio.h> // For debug prints
 
-char	*ft_resolve(char *cmd, t_sclist **a, t_sclist **b)
+void	ft_checker_error(void)
+{
+	ft_putstr_fd("Error\n", 2);
+	exit(-1);
+}
+
+static char	*ft_resolve(char *cmd, t_sclist **a, t_sclist **b)
 {
 	if (!ft_strncmp(cmd, "pb\n", 3))
 		pb(a, b, 1);
@@ -38,11 +45,11 @@ char	*ft_resolve(char *cmd, t_sclist **a, t_sclist **b)
 	else if (!ft_strncmp(cmd, "rrr\n", 4))
 		rrr(a, b, 1);
 	else
-		ft_putstr_fd("Error\n", 2);
+		ft_checker_error();
 	return (get_next_line(0));
 }
 
-void	checker_result(t_sclist **stack_a, t_sclist **stack_b, char *line)
+static void	checker_result(t_sclist **stack_a, t_sclist **stack_b, char *line)
 {
 	char	*temp;
 
@@ -61,31 +68,26 @@ void	checker_result(t_sclist **stack_a, t_sclist **stack_b, char *line)
 
 int	main(int ac, char **av)
 {
-	t_sclist	**a;
-	t_sclist	**b;
-	char		*line;
-	int			fd1;
+	t_sclist **a;
+	t_sclist **b;
+	char *cmd;
 
-	if (!is_correct_input(av, ac))
+	if (!is_correct_input(av, ac) || ac < 2)
 		return (-1);
 	a = ft_parse(av, ac);
+	if (!a)
+		return (ft_sclst_clean(a), ft_checker_error(), -1);
 	b = ft_calloc(sizeof(t_sclist), 1);
-	if (!a || !b)
-		return (ft_sclst_clean(a), ft_sclst_clean(b), -1);
-	line = NULL;
-	fd1 = open("libft/srcs/ft_atoi.c", O_RDONLY);
-	line = get_next_line(fd1);
-	if (!ft_strncmp(line, "Error", 5))
-		exit(1);
-	ft_printf("%s", line);
-	while (true)
-	{
-		line = get_next_line(fd1);
-		ft_resolve(a, b, line);
-		if (!line)
-			break ;
-		ft_printf("%s", line);
-		free(line);
-	}
-	close(fd1);
+	if (!b)
+		return (ft_sclst_clean(a), ft_sclst_clean(b), ft_checker_error(), -1);
+	cmd = get_next_line(0);
+	if (!cmd && !ft_issorted(*a))
+		ft_printf("KO\n");
+	else if (!cmd && ft_issorted(*a))
+		ft_printf("OK\n");
+	else
+		checker_result(a, b, cmd);
+	ft_sclst_clean(a);
+	ft_sclst_clean(b);
+	return (0);
 }
