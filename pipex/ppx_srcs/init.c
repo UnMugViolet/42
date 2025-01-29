b6885391ba2b991f03550e70361501fc90d5c949
unmugviolet <paul@rocketegg.systems>
2025-01-29 11:05:40 +0100

    push lol

--- pipex/ppx_srcs/init.c
+++ pipex/ppx_srcs/init.c
@@ -6,28 +6,30 @@
 /*   By: unmugviolet <unmugviolet@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 17:06:25 by unmugviolet       #+#    #+#             */
-/*   Updated: 2025/01/28 19:01:41 by unmugviolet      ###   ########.fr       */
+/*   Updated: 2025/01/29 11:05:26 by unmugviolet      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+// Print the struct: 
+// ft_printf(" in_fd = %i\n out_fd = %i\n pipefd[0] = %i pipefd[1] = %i\n cmd_count = %i\n", pipex.in_fd, pipex.out_fd, pipex.pipefd[0], pipex.pipefd[1], pipex.cmd_count);
 bool	is_struct_init(t_pipex pipex, int ac, char **av, char **env)
 {
-	pipex.in_fd = open(av[1], O_RDONLY);
-	if (pipex.in_fd == -1)
-		return (perror("Error opening file 1 : "), 0);
-	pipex.out_fd = open(av[ac - 1], O_WRONLY | O_TRUNC | O_CREAT, 0644);
-	if (pipex.out_fd == -1)
-	{
-		if (pipex.in_fd != -1)
-			close(pipex.in_fd);
-		return (perror("Error opening file 2 : "), exit(EXIT_FAILURE), 0);
-	}
-	if (pipe(pipex.pipefd) == -1)
-		return (perror("Error creating pipe : "), 0);
-	pipex.here_doc = 0;
-	pipex.cmd_count = ac - 3;
-	pipex.env = env;
-	return (1);
+    pipex.in_fd = open(av[1], O_RDONLY);
+    if (pipex.in_fd == -1)
+        return (perror("Error opening file 1 : "), 0);
+    pipex.out_fd = open(av[ac - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (pipex.out_fd == -1)
+    {
+        if (pipex.in_fd != -1)
+            close(pipex.in_fd);
+        return (perror("Error opening file 2 : "), exit(EXIT_FAILURE), 0);
+    }
+    if (pipe(pipex.pipefd) == -1)
+        return (perror("Error creating pipe : "), 0);
+    pipex.here_doc = 0;
+    pipex.cmd_count = ac - 3;
+    pipex.env = env;
+    return (1);
 }
--- pipex/ppx_srcs/input_check.c
+++ pipex/ppx_srcs/input_check.c
@@ -6,18 +6,18 @@
 /*   By: unmugviolet <unmugviolet@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 17:06:47 by unmugviolet       #+#    #+#             */
-/*   Updated: 2025/01/28 18:47:44 by unmugviolet      ###   ########.fr       */
+/*   Updated: 2025/01/28 19:23:53 by unmugviolet      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	ft_check_access_create(int ac, char **av)
+int	is_files_accessible(int ac, char **av)
 {
 	if (access(av[1], R_OK) == -1)
 		return (perror("Error file 1 : "), 0);
 	if (access(av[ac - 1], F_OK) == -1)
-		return (1);
+		return (2);
 	if (access(av[ac - 1], R_OK | W_OK) == -1)
 		return (perror("Error file 2 : "), 0);
 	return (1);
