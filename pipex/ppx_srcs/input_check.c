bf249851b369b3b20e513e099e4f2e03d94a46ce
unmugviolet <paul@rocketegg.systems>
2025-01-30 16:53:01 +0100

    Norm

--- pipex/ppx_srcs/input_check.c
+++ pipex/ppx_srcs/input_check.c
@@ -6,7 +6,7 @@
 /*   By: unmugviolet <unmugviolet@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 17:06:47 by unmugviolet       #+#    #+#             */
-/*   Updated: 2025/01/30 14:37:27 by unmugviolet      ###   ########.fr       */
+/*   Updated: 2025/01/30 16:09:50 by unmugviolet      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,13 @@
 
 void	ft_check_access(int ac, char **av)
 {
-	if (access(av[1], R_OK) == -1 && ft_strncmp(av[1], "here_doc", 9) != 0)
+	if (ft_strncmp(av[1], "here_doc", 9))
 	{
-		perror("Error file 1");
-		exit(EXIT_FAILURE);
+		if (access(av[1], R_OK) == -1)
+		{
+			perror("Error file 1");
+			exit(EXIT_FAILURE);
+		}
 	}
 	if (access(av[ac - 1], F_OK) == -1)
 		return ;
