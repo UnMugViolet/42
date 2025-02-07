15e4e474404b2320331f032d3a309742ceb91848
pjaguin <pjaguin@student.42.fr>
2025-02-07 15:01:53 +0100

    fix the strncmp in the input check

--- pipex/ppx_srcs/input_check.c
+++ pipex/ppx_srcs/input_check.c
@@ -6,7 +6,7 @@
 /*   By: pjaguin <pjaguin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 17:06:47 by unmugviolet       #+#    #+#             */
-/*   Updated: 2025/02/07 14:39:30 by pjaguin          ###   ########.fr       */
+/*   Updated: 2025/02/07 15:00:38 by pjaguin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	ft_arg_check(int ac, char **av)
 {
-	if (ac < 2 || (ac < 5 && ft_strncmp("here_doc", av[1], 9)))
+	if (ac < 2 || (ac < 5 && ft_strncmp("here_doc", av[1],9)))
 	{
 		ft_putstr_fd("Not enough arguments. ", 2);
 		ft_putstr_fd("Usage: ./pipex file1 cmd1 cmd2 ..cmd[n] file2\n", 2);
 		exit(EXIT_FAILURE);
 	}
-	else if (ac < 6 && !ft_strncmp("here_doc", av[1], 10))
+	else if (ac < 6 && !ft_strncmp("here_doc", av[1],9))
 	{
 		ft_putstr_fd("Not enough arguments. ", 2);
 		ft_putstr_fd("Usage: ./pipex here_doc LIMITER cmd1 cmd2 file2\n", 2);
