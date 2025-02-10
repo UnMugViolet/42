3ccb8cd9c7892cc3f254f479483d5ff191eab508
pjaguin <pjaguin@student.42.fr>
2025-02-10 15:46:24 +0100

    Started to work on fractal struct

--- fract-ol/srcs/init.c
+++ fract-ol/srcs/init.c
@@ -6,15 +6,14 @@
 /*   By: pjaguin <pjaguin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 11:24:23 by pjaguin           #+#    #+#             */
-/*   Updated: 2025/02/07 17:30:29 by pjaguin          ###   ########.fr       */
+/*   Updated: 2025/02/10 12:28:30 by pjaguin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fract_ol.h"
 
-void	ft_init_image(t_data *data, char *fractal, void *mlx)
+static void	ft_init_image(t_data *data, void *mlx)
 {
-	(void)fractal;
 	data->img = mlx_new_image(mlx, WIN_WIDTH, WIN_HEIGHT);
 	data->addr = mlx_get_data_addr(data->img, &data->bpp, &data->line_len,
 			&data->endian);
@@ -32,5 +31,6 @@ void	ft_init_engine(t_engine *engine, char *fractal)
 			"fract_ol");
 	if (!engine->win)
 		free(engine->mlx);
-	ft_init_image(&engine->data, fractal, engine->mlx);
+	engine->fractal.name = fractal;
+	ft_init_image(&engine->data, engine->mlx);
 }
