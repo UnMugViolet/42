eca88cf61295242d4dc9daa0af08213ba6503a71
pjaguin <pjaguin@student.42.fr>
2025-02-20 18:02:02 +0100

    Added readline

--- minishell/includes/minishell.h
+++ minishell/includes/minishell.h
@@ -6,14 +6,18 @@
 /*   By: pjaguin <pjaguin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 17:39:19 by pjaguin           #+#    #+#             */
-/*   Updated: 2025/02/20 17:42:38 by pjaguin          ###   ########.fr       */
+/*   Updated: 2025/02/20 17:59:52 by pjaguin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include "libft.h"
-#include "ft_printf.h"
+# include "libft.h"
+# include "ft_printf.h"
+
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
 #endif
--- minishell/srcs/minishell.c
+++ minishell/srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: pjaguin <pjaguin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 17:38:07 by pjaguin           #+#    #+#             */
-/*   Updated: 2025/02/20 17:43:12 by pjaguin          ###   ########.fr       */
+/*   Updated: 2025/02/20 18:01:03 by pjaguin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@ int	main(int ac, char **av, char **env)
 	(void)ac;
 	(void)av;
 	(void)env;
+	ft_printf(readline("test"));
 	return (EXIT_SUCCESS);
 }
