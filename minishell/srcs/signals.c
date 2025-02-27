1f64cae9ca8a5dc90fdf8a2200f5806498e7a842
UnMugViolet <jaguinpaul@gmail.com>
2025-02-27 10:08:15 +0100

    Addded doctring, pass norm, fix typo

---
+++ minishell/srcs/signals.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signals.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: unmugviolet <unmugviolet@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/24 12:52:21 by unmugviolet       #+#    #+#             */
+/*   Updated: 2025/02/27 10:06:49 by unmugviolet      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+// Disable the echo of crl (eg : ^C) in the terminal
+// @param void
+// @return void
+static void	ft_disable_echoctl(void)
+{
+	struct termios	term;
+
+	if (tcgetattr(STDIN_FILENO, &term) == 0)
+	{
+		term.c_lflag &= ~ECHOCTL;
+		tcsetattr(STDIN_FILENO, TCSANOW, &term);
+	}
+}
+
+// Handle the type of signal received and act accordingly
+// @param int signal
+// @return void
+static void	ft_handle_signal(int signal)
+{
+	if (signal == SIGINT)
+	{
+		write(STDOUT_FILENO, "\n", 1);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
+	else if (signal == SIGTERM)
+		ft_exit_clean(NULL);
+}
+
+// Setup the signals to be handled by the shell,
+// use the sa structure to set the flags and the handler
+// @param void
+// @return void
+void	ft_setup_signals(void)
+{
+	t_sigaction	sa;
+
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	sa.sa_handler = ft_handle_signal;
+	sigaction(SIGINT, &sa, NULL);
+	sa.sa_handler = SIG_IGN;
+	sigaction(SIGQUIT, &sa, NULL);
+	ft_disable_echoctl();
+}
