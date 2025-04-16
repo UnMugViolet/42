a7c1c89983cfd37ec0ef5171c1825a0135ebd35b
pjaguin <pjaguin@student.42.fr>
2025-04-16 10:02:25 +0200

    clean up of minishell

--- minishell/srcs/builtins/builtins.c
+++ minishell/srcs/builtins/builtins.c
@@ -3,21 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: unmugviolet <unmugviolet@student.42.fr>    +#+  +:+       +#+        */
+/*   By: pjaguin <pjaguin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 17:04:00 by fureimu           #+#    #+#             */
-/*   Updated: 2025/04/09 11:51:10 by unmugviolet      ###   ########.fr       */
+/*   Updated: 2025/04/16 10:00:21 by pjaguin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	ft_print_export(char **env, char *str, int fd)
+{
+	int	i;
+
+	i = 0;
+	if (!env)
+		return ;
+	while (env[i])
+	{
+		ft_putstr_fd(str, fd);
+		ft_putendl_fd(env[i], fd);
+		i++;
+	}
+}
+/*
+	Contain the rules and functions for all the builtins executed in the
+	parent process. It will also update the last exit value of the shell.
+	@param	data
+	@param	cmd
+	@return	1 if a built-in command was executed, 0 otherwise.
+*/
 int	ft_exec_parent_builtins(t_data *data, char **cmd)
 {
 	if (!ft_strncmp(*cmd, "exit", 5))
 		return (ft_exit(data, cmd), 1);
 	else if (!ft_strncmp(*cmd, "cd", 3))
 		return (ft_update_last_exit_value(data, ft_cd(data, cmd)), 1);
+	else if (!ft_strncmp(*cmd, "export", 7))
+		return (ft_print_export(data->env, "export ", STDOUT_FILENO), 1);
 	else if (!ft_strncmp(*cmd, "export", 7) && cmd[1])
 		return (ft_update_last_exit_value(data, ft_create_env_var(data,
 					cmd[1])), 1);
@@ -27,6 +50,13 @@ int	ft_exec_parent_builtins(t_data *data, char **cmd)
 	return (0);
 }
 
+/*
+	Contain the rules and functions for all the builtins executed in the
+	child process. It will also update the last exit value of the shell.
+	@param	data
+	@param	cmd
+	@return	1 if a built-in command was executed, 0 otherwise.
+*/
 int	ft_exec_child_builtins(t_data *data, char **cmd)
 {
 	if (!ft_strncmp(*cmd, "pwd", 4))
