abd0db72a9a064c808d9e41db71ef40587ff0533
UnMugViolet <jaguinpaul@gmail.com>
2025-04-19 11:13:41 +0200

    Fix all forks order is not that bad after all

--- philosophers/srcs/actions.c
+++ philosophers/srcs/actions.c
@@ -6,35 +6,35 @@
 /*   By: unmugviolet <unmugviolet@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 11:38:11 by pjaguin           #+#    #+#             */
-/*   Updated: 2025/04/18 19:49:09 by unmugviolet      ###   ########.fr       */
+/*   Updated: 2025/04/19 11:09:02 by unmugviolet      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include "utils.h"
 
-// TODO add the function to do tiny usleeps instead of one large
-
 /*
-	Get the order of the forks for the philosopher in order to avoid deadlocks.
+	Performs mico actions to avoid waiting for all the time if a philosopher
+	died. This function will split the time to sleep into 50 parts and sleep
+	for each part. This way, if a philosopher dies, the program will not
+	wait.
 	@param philo: the philosopher structure
-	@param forks: the forks mutexes
+	@param time: the time to sleep
 	@return: void
 */
-void	ft_get_forks_order(t_philo *philo, pthread_mutex_t **forks)
+void ft_tiny_usleep(t_philo *philo, size_t time)
 {
-	if (!philo->r_fork || !philo->l_fork)
-    	return;
-	if (philo->id % 2 == 1)
-	{
-		forks[0] = philo->l_fork;
-		forks[1] = philo->r_fork;
-	}
-	else
-	{
-		forks[0] = philo->r_fork;
-		forks[1] = philo->l_fork;
-	}
+    size_t chunk;
+    chunk = 10;
+    while (time > 0)
+    {
+        if (ft_is_dead_flag(philo))
+            return;
+        if (chunk > time)
+            chunk = time;
+        ft_usleep(chunk);
+        time -= chunk;
+    }
 }
 
 void	ft_think(t_philo *philo)
@@ -45,32 +45,29 @@ void	ft_think(t_philo *philo)
 void	ft_dream(t_philo *philo)
 {
 	print_message(philo, "is sleeping");
-	ft_usleep(philo->time_to_sleep);
+	ft_tiny_usleep(philo, philo->time_to_sleep);
 }
 
 void	ft_eat(t_philo *philo)
 {
-	pthread_mutex_t *forks[2];
-	
-	ft_get_forks_order(philo, forks);
-	pthread_mutex_lock(forks[0]);
-	print_message(philo, "has taken a fork");
-	if (philo->nb_philo == 1)
-	{
-		ft_usleep(philo->time_to_die);
-		pthread_mutex_unlock(forks[0]);
-		return ;
-	}
-	pthread_mutex_lock(forks[1]);
-	print_message(philo, "has taken a fork");
-	philo->is_eating = true;
-	print_message(philo, "is eating");
-	pthread_mutex_lock(philo->meal_lock);
-	philo->last_meal = ft_get_time();
-	philo->meals_eaten++;
-	pthread_mutex_unlock(philo->meal_lock);
-	ft_usleep(philo->time_to_eat);
-	philo->is_eating = false;
-	pthread_mutex_unlock(forks[1]);
-	pthread_mutex_unlock(forks[0]);
+    pthread_mutex_lock(philo->r_fork);
+    print_message(philo, "has taken a fork");
+    if (philo->nb_philo == 1)
+    {
+        ft_usleep(philo->time_to_die);
+        pthread_mutex_unlock(philo->r_fork);
+        return ;
+    }
+    pthread_mutex_lock(philo->l_fork);
+    print_message(philo, "has taken a fork");
+    philo->is_eating = true;
+    print_message(philo, "is eating");
+    pthread_mutex_lock(philo->meal_lock);
+    philo->last_meal = ft_get_time();
+    philo->meals_eaten++;
+    pthread_mutex_unlock(philo->meal_lock);
+    ft_usleep(philo->time_to_eat);
+    philo->is_eating = false;
+    pthread_mutex_unlock(philo->l_fork);
+    pthread_mutex_unlock(philo->r_fork);
 }
