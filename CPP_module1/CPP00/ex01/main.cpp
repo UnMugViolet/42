774d7d65e6c6d2da090ab05003f315ecdd5c10b9
pjaguin <pjaguin@student.42.fr>
2025-05-06 18:34:24 +0200

    Started ex01 CPP00

--- CPP_module1/CPP00/ex01/main.cpp
+++ CPP_module1/CPP00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pjaguin <pjaguin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 12:31:23 by pjaguin           #+#    #+#             */
-/*   Updated: 2025/05/06 16:16:48 by pjaguin          ###   ########.fr       */
+/*   Updated: 2025/05/06 18:03:59 by pjaguin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int main(void)
 		else if (command == "ADD")
 			add_contact(phone_book);
 		else if (command == "SEED")
-			contact_seeder(phone_book); // REMOVE LATER
+			contact_seeder(phone_book);
 		else if (command == "SEARCH")
 			phone_book.search_contact();
 		else
---
+++ CPP_module1/CPP00/ex02/Account.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pjaguin <pjaguin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/06 17:20:50 by pjaguin           #+#    #+#             */
+/*   Updated: 2025/05/06 18:33:39 by pjaguin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Account.hpp"
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit)
+{
+	_accountIndex = _nbAccounts++;
+	_amount = initial_deposit;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+	_totalAmount += initial_deposit;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created" << std::endl;
+}
+
+Account::displayAccountsInfos(void)
+{
+	_displayTimestamp();
+	std::cout << "accounts:" << _nbAccounts << ";total:" << _totalAmount
+			  << ";deposits:" << _totalNbDeposits
+			  << ";withdrawals:" << _totalNbWithdrawals << std::endl;
+}
+
+Account::~Account(void)
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";closed" << std::endl;
+}
