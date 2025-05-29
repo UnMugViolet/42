c067f03fb9953459660109483d9f03faee5c4fa1
Fureimu <yguinio@student.42.fr>
2025-05-29 16:13:54 +0200

    norm

--- cube3d/srcs/events.c
+++ cube3d/srcs/events.c
@@ -6,7 +6,7 @@
 /*   By: yguinio <yguinio@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 18:20:00 by pjaguin           #+#    #+#             */
-/*   Updated: 2025/05/28 15:20:47 by yguinio          ###   ########.fr       */
+/*   Updated: 2025/05/29 16:13:32 by yguinio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	ft_key_press(int keycode, t_engine *engine)
 {
-    if (keycode == XK_Escape)
-        ft_destroy_event(engine);
-    return (EXIT_SUCCESS);
+	if (keycode == XK_Escape)
+		ft_destroy_event(engine);
+	return (EXIT_SUCCESS);
 }
 
 int	ft_destroy_event(t_engine *engine)
--- cube3d/srcs/main.c
+++ cube3d/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: yguinio <yguinio@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 18:02:14 by pjaguin           #+#    #+#             */
-/*   Updated: 2025/05/29 14:49:49 by yguinio          ###   ########.fr       */
+/*   Updated: 2025/05/29 16:13:39 by yguinio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ int	main(int ac, char **av)
 	if (!is_valid_input(ac, av, &engine))
 		return (ft_display_usage(), EXIT_FAILURE);
 	ft_init_window(&engine, av[1]);
-	mlx_put_image_to_window(engine.mlx, engine.win, engine.data.textures[0].img_ptr, 0, 0);
+	mlx_put_image_to_window(engine.mlx, engine.win,
+		engine.data.textures[0].img_ptr, 0, 0);
 	mlx_hook(engine.win, KeyPress, KeyPressMask, &ft_key_press, &engine);
 	mlx_hook(engine.win, DestroyNotify, 0, &ft_destroy_event, &engine);
 	mlx_loop(engine.mlx);
