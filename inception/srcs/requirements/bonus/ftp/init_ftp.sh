12d9420c6c972d8e884c09f2a8f5a3d65e87945b
UnMugViolet <jaguinpaul@gmail.com>
2025-07-07 17:44:44 +0200

    Ftp is working

--- inception/srcs/requirements/bonus/ftp/init_ftp.sh
+++ inception/srcs/requirements/bonus/ftp/init_ftp.sh
@@ -7,14 +7,6 @@ fi
 
 service vsftpd start
 
-# Check if the FTP server is running
-if pgrep -x "vsftpd" > /dev/null; then
-	echo "FTP server is running."
-else
-	echo "FTP server failed to start."
-	exit 1
-fi
-
 if ! id "$FTP_USER" &>/dev/null; then
     adduser $FTP_USER --disabled-password --gecos ""
 fi
@@ -26,8 +18,8 @@ mkdir -p /home/$FTP_USER/ftp
 chown nobody:nogroup /home/$FTP_USER/ftp
 chmod a-w /home/$FTP_USER/ftp
 
-mkdir -p /home/$FTP_USER/ftp/files
-chown $FTP_USER:$FTP_USER /home/$FTP_USER/ftp/files
+mkdir -p /home/$FTP_USER/ftp/wordpress
+chown $FTP_USER:$FTP_USER /home/$FTP_USER/ftp/wordpress
 
 sed -i "s/#write_enable=YES/write_enable=YES/" /etc/vsftpd.conf
 sed -i "s/#chroot_local_user=YES/chroot_local_user=YES/" /etc/vsftpd.conf
@@ -42,3 +34,11 @@ pasv_max_port=40010
 userlist_file=/etc/vsftpd.userlist
 " >> /etc/vsftpd.conf
 
+# Restart vsftpd to apply configuration changes
+service vsftpd restart
+echo "FTP server is set up and running."
+
+# Monitor logs
+touch /var/log/vsftpd.log
+chmod 644 /var/log/vsftpd.log
+tail -f /var/log/vsftpd.log
