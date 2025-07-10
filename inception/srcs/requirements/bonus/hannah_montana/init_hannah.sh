cf5b178a5c85490c2b5ca0f4bf5e93b828a7bf1d
pjaguin <pjaguin@student.42.fr>
2025-07-10 11:00:48 +0200

    Fix hannah

--- inception/srcs/requirements/bonus/hannah_montana/init_hannah.sh
+++ inception/srcs/requirements/bonus/hannah_montana/init_hannah.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Always copy the latest web files to the mounted volume
-echo "Copying latest web files to /var/www/html..."
-rm -rf /var/www/html/*
-cp -r /tmp/web-files/* /var/www/html/
-chown -R www-data:www-data /var/www/html
-chmod -R 755 /var/www/html
-
-rm -rf /tmp/web-files
+echo "Starting Apache server..."
 
 exec apache2ctl -D FOREGROUND
