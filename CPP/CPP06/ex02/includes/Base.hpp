3ef18d0c3055a72a6a5d55e2c6bd5387f999e7cb
unmugviolet <jaguinpaul@gmail.com>
2025-07-25 11:02:02 +0200

    Finished ex02

---
+++ CPP/CPP06/ex02/includes/Base.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Base.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: unmugviolet <unmugviolet@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/25 10:33:48 by unmugviolet       #+#    #+#             */
+/*   Updated: 2025/07/25 10:53:52 by unmugviolet      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#ifndef BASE_HPP
+#define BASE_HPP
+
+#include <iostream>
+
+class Base
+{
+	public:
+		virtual ~Base();
+};
+
+#endif
---
+++ CPP/CPP06/ex02/srcs/main.cpp
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: unmugviolet <unmugviolet@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/23 16:37:12 by unmugviolet       #+#    #+#             */
+/*   Updated: 2025/07/25 10:58:30 by unmugviolet      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+#include <cstdlib>
+#include <ctime>
+
+Base	*generate()
+{
+	size_t random = rand() % 3;
+	if (random == 0)
+		return new A;
+	else if (random == 1)
+		return new B;
+	else
+		return new C;
+}
+
+void identify(Base *p)
+{
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+	else
+		std::cout << "Unknown type" << std::endl;
+}
+
+void identify(Base &p)
+{
+	try {
+		A &a = dynamic_cast<A&>(p);
+		(void)a;
+		std::cout << "A" << std::endl;
+	} catch (std::exception &e) {}
+
+	try {
+		B &b = dynamic_cast<B&>(p);
+		(void)b;
+		std::cout << "B" << std::endl;
+	} catch (std::exception &e) {}
+
+	try {
+		C &c = dynamic_cast<C&>(p);
+		(void)c;
+		std::cout << "C" << std::endl;
+	} catch (std::exception &e) {}
+}
+
+
+int main()
+{
+	srand(time(0));
+	Base *base = generate();
+	Base &ref = *base;
+	identify(base);
+	identify(ref);
+	delete base;
+}
