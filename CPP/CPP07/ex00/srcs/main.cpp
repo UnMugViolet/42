a2412a0f36da484abfbb19444028605044b489f0
unmugviolet <jaguinpaul@gmail.com>
2025-07-27 07:40:43 +0200

    Added the subject main

--- CPP/CPP07/ex00/srcs/main.cpp
+++ CPP/CPP07/ex00/srcs/main.cpp
@@ -6,14 +6,37 @@
 /*   By: unmugviolet <unmugviolet@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 14:16:34 by pjaguin           #+#    #+#             */
-/*   Updated: 2025/07/25 17:49:18 by unmugviolet      ###   ########.fr       */
+/*   Updated: 2025/07/27 07:40:02 by unmugviolet      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "operations.tpp"
 #include "dict.hpp"
 
-int main()
+int main( void ) {
+
+	int a = 2;
+	int b = 3;
+	
+	::swap( a, b );
+	
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	
+	return 0;
+}
+
+/* int main()
 {
 	double	A = -12.7845;
 	double	B = 4862.1754;
@@ -36,4 +59,7 @@ int main()
 	
 	std::cout << BOLD << "A value: " << NEUTRAL << A << std::endl;
 	std::cout << BOLD << "B value: " << NEUTRAL << B << std::endl;
-}
+
+	return 0;
+
+} */
