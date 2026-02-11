5c20b7ed00d2b8a86274e562160918fa7649853e
unmugviolet <jaguinpaul@gmail.com>
2026-02-11 17:36:44 +0100

    remove useless counter

--- libasm/srcs/ft_strlen.s
+++ libasm/srcs/ft_strlen.s
@@ -5,14 +5,13 @@ ft_strlen:
 	xor rax, rax          ; Initialize length counter to 0
 
 _loop:
-	cmp BYTE [rdi], 0    ; Check if the current byte is the null terminator
-	je _return           ; If it is, go to the return label
-	inc rax              ; Increment the length counter
-	inc rdi              ; Move to the next byte
-	jmp _loop            ; Jump back to loop 
+	cmp BYTE [rdi + rax], 0     ; Check if the current byte is the null terminator
+	je _return           		; If it is, go to the return label
+	inc rax              		; Increment the length counter
+	jmp _loop            		; Jump back to loop 
 
 _return:
-	ret                   ; Return the length in RAX
+	ret                   		; Return the length in RAX
 
 ; Mark stack as non-executable
 section .note.GNU-stack
